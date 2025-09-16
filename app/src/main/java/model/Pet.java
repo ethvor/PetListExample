f4ef23b20043b770c47048ae990bec00a78151bd
ethvor <contactethanv@gmail.com>
2025-09-16 11:38:13 -0500

    constructors, getters setters, arrays.xml resource file (empty)

--- app/src/main/java/model/Pet.java
+++ app/src/main/java/model/Pet.java
@@ -7,4 +7,39 @@ public class Pet {
     String breed;
     int age;
 
+    //constructor
+    public Pet(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    //default constructor
+    public Pet() {
+    }
+
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
